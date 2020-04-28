76bf5d4068d4c3c76877d2c0bf4971d547ef65e2
Jamison Cozart <jamisoncozart@gmail.com>
2020-04-28 07:24:21 -0700

    Add READ route based on unique note id

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,4 +1,19 @@
+var ObjectID = require('mongodb').ObjectID;
+
 module.exports = function(app, db) {
+  // READ NOTE
+  app.get('/notes/:id', (req, res) => {
+    const id = req.params.id;
+    const details = { '_id': new ObjectID(id) };
+    db.collection('notes').findOne(details, (err, item) => {
+      if(err) {
+        res.send({'error': 'An error has occurred'});
+      } else {
+        res.send(item);
+      }
+    });
+  });
+  // CREATE NEW NOTE
   app.post('/notes', (req, res) => {
     const note = {
       title: req.body.title,
